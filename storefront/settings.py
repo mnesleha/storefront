48981d1578ef70e7d17fbebecaa499dd74ccdad5
Marek Nesleha <mnesleha@gmail.com>
2025-11-20 14:20:33 +0100

    Implemented data model.

--- storefront/settings.py
+++ storefront/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'playground',
+    'store',
+    'tags'
 ]
 
 MIDDLEWARE = [
---
+++ tags/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+# Create your models here.
+
+
+class LikedItem(models.Model):
+    # what user liked what object
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class TaggedItem(models.Model):
+    # what tag applies to what object
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
