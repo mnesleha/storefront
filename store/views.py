7202df315ce4523410a143aca12ec914c04096b6
Marek Nesleha <mnesleha@gmail.com>
2025-11-30 11:38:57 +0100

    Enhancement to Django REST API

--- store/views.py
+++ store/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.decorators import api_view
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework import status
+from store.filters import ProductFilter
+from store.pagination import DefaultPagination
 from .models import Product, Collection, OrderItem, Review
 from django.db.models import Count
 from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
@@ -16,6 +21,11 @@ from .serializers import ProductSerializer, CollectionSerializer, ReviewSerializ
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = ProductFilter
+    pagination_class = DefaultPagination
+    search_fields = ['title', 'description']
+    ordering_fields = ['unit_price', 'last_update']
 
     def get_serializer_context(self):
         return {'request': self.request}
